f01a5aa25777b11b25f88cd6a666d04bfd6e24dc
Arshiajj7 <Abolfazl.jbl82@gmail.com>
2024-11-20 18:19:40 +0000

    js code Commit.

--- script.js
+++ script.js
@@ -4,10 +4,14 @@ function appendToDisplay(input) {
   display.value += input;
 }
 
-function clearDisplay() {
-  display.value = "";
-}
-
 function calculate() {
   display.value = eval(display.value);
+}
+
+function clearLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function allClear() {
+  display.value = "";
 }
