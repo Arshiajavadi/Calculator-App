2cbe1f706700d330958e0586ee630b0cf831a027
Arshiajj7 <Abolfazl.jbl82@gmail.com>
2024-11-23 17:45:56 +0000

    javaScript Code update Commit.

--- script.js
+++ script.js
@@ -1,17 +1,37 @@
 const display = document.getElementById("display");
+const calculation = document.querySelector('.calculation');
 
 function appendToDisplay(input) {
-  display.value += input;
+  if (display.value === "0") {
+    display.value = input;
+  } else {
+    display.value += input;
+  }
 }
 
 function calculate() {
-  display.value = eval(display.value);
+  if (display.value === null || display.value === '') {
+    display.value = 0;
+    calculation.textContent = null;
+  }
+  else {
+    try {
+      const result = eval(display.value);
+      calculation.textContent = display.value;
+      display.value = result;
+    } catch (error) {
+      display.value = "Error";
+    };
+  }
 }
+
 
 function clearLast() {
   display.value = display.value.slice(0, -1);
+  calculation.textContent = '';
 }
 
 function allClear() {
   display.value = "";
+  calculation.textContent = '';
 }
