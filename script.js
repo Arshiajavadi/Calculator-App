797f80da46999337c5018ff1cd5dffa07b6b7f09
Arshiajj7 <Abolfazl.jbl82@gmail.com>
2024-11-24 23:10:25 +0000

    calculator update Commit.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const display = document.getElementById("display");
-const calculation = document.querySelector('.calculation');
+const calculation = document.querySelector(".calculation");
+const buttons = document.querySelectorAll("button");
+const clear = document.querySelector("#clear");
+
+function tap() {
+  const audio = new Audio("audios/tap-output.mp3");
+  audio.play();
+}
+
+for (let button of buttons) {
+  if (button.id !== "clear") {
+    button.addEventListener("click", tap);
+  }
+}
+
+function clearTap() {
+  const audio = new Audio("audios/eraseTap-output.mp3");
+  audio.play();
+}
+
+clear.addEventListener("click", clearTap);
 
 function appendToDisplay(input) {
   if (display.value === "0") {
@@ -10,28 +30,26 @@ function appendToDisplay(input) {
 }
 
 function calculate() {
-  if (display.value === null || display.value === '') {
+  if (display.value === "" || display.value === "0") {
     display.value = 0;
-    calculation.textContent = null;
-  }
-  else {
+    calculation.textContent = "";
+  } else {
     try {
       const result = eval(display.value);
       calculation.textContent = display.value;
       display.value = result;
     } catch (error) {
       display.value = "Error";
-    };
+    }
   }
 }
 
-
 function clearLast() {
   display.value = display.value.slice(0, -1);
-  calculation.textContent = '';
+  calculation.textContent = "";
 }
 
 function allClear() {
   display.value = "";
-  calculation.textContent = '';
+  calculation.textContent = "";
 }
