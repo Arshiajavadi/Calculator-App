a8cf8e09bc72a3ac41e6eb30c0b74d3c96309e41
Arshiajj7 <Abolfazl.jbl82@gmail.com>
2024-11-25 23:14:39 +0000

    Final result Commit.

--- script.js
+++ script.js
@@ -1,26 +1,32 @@
+//Get all elements
 const display = document.getElementById("display");
 const calculation = document.querySelector(".calculation");
 const buttons = document.querySelectorAll("button");
 const clear = document.querySelector("#clear");
 
+//Function to play tap sound
 function tap() {
   const audio = new Audio("audios/tap-output.mp3");
   audio.play();
 }
 
+//Add click event to all buttons except the clear button
 for (let button of buttons) {
   if (button.id !== "clear") {
     button.addEventListener("click", tap);
   }
 }
 
+//Function to play clear tap sound
 function clearTap() {
   const audio = new Audio("audios/eraseTap-output.mp3");
   audio.play();
 }
 
+//Add click event to cleartap button
 clear.addEventListener("click", clearTap);
 
+//Function to append input to the display
 function appendToDisplay(input) {
   if (display.value === "0") {
     display.value = input;
@@ -29,6 +35,7 @@ function appendToDisplay(input) {
   }
 }
 
+//Function to do the calculation
 function calculate() {
   if (display.value === "" || display.value === "0") {
     display.value = 0;
@@ -44,11 +51,13 @@ function calculate() {
   }
 }
 
+//Function to clear the last input
 function clearLast() {
   display.value = display.value.slice(0, -1);
   calculation.textContent = "";
 }
 
+//Function to clear all value from input
 function allClear() {
   display.value = "";
   calculation.textContent = "";
